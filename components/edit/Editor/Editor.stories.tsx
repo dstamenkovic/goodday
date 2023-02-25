fad343a1581682b6a4d5361bc085c03be743c81d
dstamenkovic <dusanstameni@gmail.com>
2023-02-25 20:06:30 +0100

    editor story update

--- components/edit/Editor/Editor.stories.tsx
+++ components/edit/Editor/Editor.stories.tsx
@@ -11,4 +11,12 @@ export default meta
 
 type Story = StoryObj<typeof Editor>
 
-export const Default: Story = {}
+export const Default: Story = {
+  decorators: [
+    Story => (
+      <div className="my-10 mx-4">
+        <Story />
+      </div>
+    ),
+  ],
+}
