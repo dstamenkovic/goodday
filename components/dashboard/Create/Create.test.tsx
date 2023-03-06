88dd6f5d9a746ebebe0c54db6466610af071e14d
dstamenkovic <dusanstameni@gmail.com>
2023-03-06 16:11:22 +0100

    create comp test update

--- components/dashboard/Create/Create.test.tsx
+++ components/dashboard/Create/Create.test.tsx
@@ -1,11 +1,18 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 import Create from './Create'
 
 describe('Create', () => {
   it('renders correctly', () => {
-    render(<Create />)
+    render(<Create onClickHandler={jest.fn()} />)
     expect(screen.getByText('Create')).toBeInTheDocument()
+  })
+
+  it('calls onClickHandler when clicked', () => {
+    const onClickHandler = jest.fn()
+    const { container } = render(<Create onClickHandler={onClickHandler} />)
+    fireEvent.click(container.querySelector('.item')!)
+    expect(onClickHandler).toHaveBeenCalled()
   })
 })
